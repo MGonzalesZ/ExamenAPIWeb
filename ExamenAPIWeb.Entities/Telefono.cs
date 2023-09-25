2ec46cbc0f505e37421e644042a9896f528525b4
fermin <fergta54@gmail.com>
2023-09-25 15:28:45 -0400

    CAMBIO DE CLASE A ENTIDADES

---
+++ ExamenAPIWeb.Entities/Telefono.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExamenAPIWeb.DAL.DataContext
+{
+    public partial class Telefono
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? NumeroTelefonico { get; set; }
+    }
+}
