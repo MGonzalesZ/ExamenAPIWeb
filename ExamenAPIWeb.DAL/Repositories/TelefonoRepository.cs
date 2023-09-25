270cb11f918f4f64f96327433575fa03610a56b7
fermin <fergta54@gmail.com>
2023-09-25 15:26:17 -0400

    repositories DAL

---
+++ ExamenAPIWeb.DAL/Repositories/TelefonoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenAPIWeb.DAL.Repositories
+{
+    internal class TelefonoRepository
+    {
+    }
+}
